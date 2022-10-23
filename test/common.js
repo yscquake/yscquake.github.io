2971086392805f979fe2e307acd20b46db2a6949
YSC <98032930+yscquake@users.noreply.github.com>
2022-10-23 21:19:54 +0900

    Delete test directory

--- test/common.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $("#header").load("./header.html");
-    $("#side").load("./side.html");
-    $("#footer").load("./footer.html");
-    $("#ob-table").load("./leaflet.table.html");
-});
